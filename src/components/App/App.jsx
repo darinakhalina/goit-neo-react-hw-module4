81b266ac23febf6a604615980c4588383a23c38e
Darina Khalina <darina.khalina@namecheap.com>
2024-09-02 20:45:06 +0300

    feat(hw): add SearchBar component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,35 +1,39 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { fetchGallery } from '../../api/api-gallery';
-import css from './App.module.css';
+import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
   const [images, setImages] = useState([]);
-  // const [totalPages, setTotalPages] = useState(-1);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [error, setError] = useState(null);
-  // const [page, setPage] = useState(1);
-  // const [query, setQuery] = useState('');
+  const [totalPages, setTotalPages] = useState(-1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
   // const [selectedImage, setSelectedImage] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await fetchGallery('test', 1);
-        setImages(data.results);
-      } catch (error) {
-        console.log('error', error);
-      } finally {
-        console.log('finally');
-      }
-    };
-    fetchData();
-  }, []);
+  const handleSearch = async query => {
+    setImages([]);
+    setPage(1);
+    setError(false);
+    setIsLoading(true);
+
+    try {
+      const data = await fetchGallery(query, 1);
+      setQuery(query);
+      setImages(data.results);
+      setTotalPages(data.total_pages);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   console.log(images);
   return (
-    <div className={css.app}>
-      <div>TEST</div>
-    </div>
+    <>
+      <SearchBar onSubmit={handleSearch} />
+    </>
   );
 }
 
